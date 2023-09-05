efa5c34772a11aa4aadd8ded44a7f604c7cb6d9b
Alaa Shaban <devalaashaban29@gmail.com>
2023-09-06 01:04:15 +0300

    Remove Durations component and associated import

--- src/page/Natiq.jsx
+++ src/page/Natiq.jsx
@@ -3,7 +3,6 @@ import { useContextNatiq } from "../context/NatiqContext";
 import Loading from "../ui/Loading";
 import NatiqForm from "../features/Natiq/NatiqForm";
 import NatiqAudio from "../features/Natiq/NatiqAudio";
-import NatiqDurations from "../features/Natiq/NatiqDurations";
 
 export default function Natiq() {
   const { isLoading } = useContextNatiq();
